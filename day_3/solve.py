2164c4f5a8903e66dc35154ebfb1b458c804f276
Esteban Vincent <esteban.vincent@cellenza.com>
2023-12-03 14:31:34 +0100

    innit day3

---
+++ day_3/solve.py
@@ -0,0 +1,12 @@
+def read_file(file_path: str) -> list:
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+    return lines
+
+
+def solve_part_1(lines: list) -> int:
+    pass
+
+
+def solve_part_2(lines: list) -> int:
+    pass
---
+++ test/test_day_3.py
@@ -0,0 +1,34 @@
+import unittest
+from day_2.solve import read_file, solve_part_1, solve_part_2
+
+
+class Test_Day2(unittest.TestCase):
+    def setUp(self):
+        # Read the input files
+        self.input_df = read_file("day_3/input.txt")
+        self.example_1_df = read_file("day_3/example_1.txt")
+        self.example_2_df = self.read_file("day_3/example_2.txt")
+
+    def read_file(self, file_path):
+        with open(file_path, "r") as file:
+            return file.readlines()
+
+    def test_solve_part_1_with_example_1(self):
+        result = solve_part_1(self.example_1_df)
+        self.assertEqual(result, 8)
+
+    def test_solve_part_1_with_input(self):
+        result = solve_part_1(self.input_df)
+        self.assertEqual(result, 2076)
+
+    def test_solve_part_2_with_example_2(self):
+        result = solve_part_2(self.example_1_df)
+        self.assertEqual(result, 2286)
+
+    def test_solve_part_2_with_input(self):
+        result = solve_part_2(self.input_df)
+        self.assertEqual(result, 70950)
+
+
+if __name__ == "__main__":
+    unittest.main()
